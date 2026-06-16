b30d799fee43055fc5ed7cf685a6e814ae50d821
Brian Hechinger <wonko@4amlunch.net>
2026-06-16 15:49:04 +0100

    fix: add rust analyzer dev shell tools

--- shell.nix
+++ shell.nix
@@ -1,6 +1,9 @@
 {
   mkShell,
   callPackage,
+  rustPlatform,
+  clippy,
+  rust-analyzer,
   libjack2,
   pipewire,
   llvmPackages,
@@ -15,7 +18,12 @@ mkShell {
     pipewire
     llvmPackages.libclang
   ];
+  nativeBuildInputs = [
+    clippy
+    rust-analyzer
+  ];
 
   LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
   BINDGEN_EXTRA_CLANG_ARGS = "-isystem ${stdenv.cc.libc.dev}/include";
+  RUST_SRC_PATH = "${rustPlatform.rustLibSrc}";
 }
